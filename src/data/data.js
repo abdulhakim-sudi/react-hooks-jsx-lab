149379762d601abd5b9baf34c47541b934ec7dcf
ABDULHAKIM <abdulhakimsudi27@gmail.com>
2025-04-12 09:38:56 +0300

    Completed JSX Lab: Updated NavBar, Home, and About components

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "hakeem";
+const city = "uk";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
